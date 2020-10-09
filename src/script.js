ae354f7b01177bf37cd96df977fc12117e632762
Hubert <hubertkasprzycki6@gmail.com>
2020-10-09 18:26:19 +0200

    Refactoring vanilla javascript to GSAP library with ScrollTrigger usage

--- src/script.js
+++ src/script.js
@@ -2,10 +2,20 @@ const hero_img_2 = document.querySelector('.hero__img2');
 const hero_img_1 = document.querySelector('.hero__img1');
 const hero_text = document.querySelector('.hero__text');
 
-window.addEventListener('scroll', () => {
-    const offsetY = window.scrollY;
+gsap.registerPlugin(ScrollTrigger);
 
-    hero_img_2.style.transform = `translateY(${offsetY*0.35}px)`;
-    hero_img_1.style.transform = `translateY(${offsetY*0.25}px)`;
-    hero_text.style.transform = `translateX(-50%) translateY(${offsetY*0.65}px)`;
+const tl = gsap.timeline({
+	defaults: {
+		ease: 'none',
+	},
+  scrollTrigger: {
+    trigger: '.hero',
+    scrub: true,
+    start: 'top top',
+		end: 'bottom top',
+  },
 });
+
+tl.to(hero_text, { y: 1000 })
+  .to(hero_img_2, { y: 700 }, 0)
+  .to(hero_img_1, { y: 350 }, 0);
